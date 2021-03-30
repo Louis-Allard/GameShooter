b87a13bc277c6b68e633e373a0b20d94ed7d9ebd
Louis Allard <la.devweb@yahoo.com>
2021-03-30 16:48:21 +0200

    Add Elements file.

---
+++ e/Elements.py
@@ -0,0 +1,14 @@
+import pygame
+from e.variables import *
+
+class Elements(pygame.sprite.Sprite):
+    el01 = pygame.image.load("./sprites/el01.png").convert_alpha()
+    el02 = pygame.image.load("./sprites/el02.png").convert_alpha()
+    el03 = pygame.image.load("./sprites/el03.png").convert_alpha()
+    el04 = pygame.image.load("./sprites/el04.png").convert_alpha()
+
+    def elmnts(self,elmnts_x,elmnts_y,espace_elemnts):
+        surface.blit(self.el01,(elmnts_x,elmnts_y + espace_elemnts))
+        surface.blit(self.el02,(elmnts_x,elmnts_y + el01_h + espace_elemnts - 105))
+        surface.blit(self.el03,(elmnts_x,elmnts_y + el02_h + espace_elemnts - 300))
+        surface.blit(self.el04,(elmnts_x,elmnts_y + el03_h + espace_elemnts - 90))
