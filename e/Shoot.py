1889f5b5a2ca93272137867cae5587906673f457
Louis Allard <la.devweb@yahoo.com>
2021-03-30 18:08:13 +0200

    Add file.

---
+++ e/Shoot.py
@@ -0,0 +1,15 @@
+import pygame
+from e.variables import *
+
+class Shoot(pygame.sprite.Sprite):  
+    bullet = pygame.image.load("./sprites/bullet.png").convert_alpha()
+
+    def goBullet(self,bullet_x,bullet_y,enable):
+        if enable == 1:
+            self.bullet.set_alpha(200)
+        elif enable == 0:
+            self.bullet.set_alpha(0)
+        else:
+            print("WARNING BULLET ALPHA")    
+        surface.blit(self.bullet,(bullet_x,bullet_y))
+        print(bullet_x)
