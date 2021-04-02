edd5e06db7e4a7dfa9b7ce81661ae8ead8057d62
Louis Allard <la.devweb@yahoo.com>
2021-04-02 16:50:35 +0200

    Add bullet file.

---
+++ e/Bullet.py
@@ -0,0 +1,15 @@
+import pygame
+from e.Personnage import *
+from e.variables import *
+
+pygame.display.set_caption("GameShooter")
+
+class Bullet(pygame.sprite.Sprite):
+    def __init__(self,pos_x,pos_y):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = pygame.Surface((30,5))
+        self.image.fill((255,0,0))
+        self.rect = self.image.get_rect(center = (pos_x,pos_y))
+
+    def update(self):
+        self.rect.x  += 5
